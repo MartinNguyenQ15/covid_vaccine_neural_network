c6591e1fec3bdc773b6dfba2c2388429d33826d9
Vladimir Ventura <vladventura@outlook.es>
2023-04-28 18:17:03 -0400

    Adding driver for logistic regression. It is working, but our problem is not a classification one
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

---
+++ mid-project/logistic-regression-driver.py
@@ -0,0 +1,26 @@
+import numpy as np
+from loaders import load_by_year, load_by_county
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from logistic_regression import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+without, feature_range = load_by_county(selected_columns=['county', 'total_doses', 'cumulative_total_doses'])
+print(without)
+X = without.iloc[:, 1:].values
+y = without.iloc[:,0].values
+
+scaler = MinMaxScaler(feature_range=feature_range)
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.5, random_state=42, )
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train, epochs=500, lr=0.5)
+y_pred = logreg.predict(X_test)
+
+y_test = np.array(y_test).astype("float32")
+y_pred = np.array(y_pred).astype("float32")
+print(classification_report(y_test, y_pred))
+print("Confusion Matrix")
+print(confusion_matrix(y_test, y_pred))
