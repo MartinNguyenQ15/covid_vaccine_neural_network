15ebd80f0056a39a53863bc5b6ed9f48284df316
Vladimir Ventura <vladventura@outlook.es>
2023-04-29 11:11:31 -0400

    Adding driver to visualize theoretical data for the mid project
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- mid-project/visualize-driver.py
+++ mid-project/visualize-driver.py
@@ -1,30 +1,18 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
+from loaders import load_by_year, load_by_county
 from visualize import show_heatmap
 
-df = pd.read_csv('./cali_dataset.csv')
-
-# Issue to resolve with f1_score if more than 2 cities
-county_list = [
-    'Calaveras',
-    # 'Lassen',
-    # 'Alpine',
-    'Plumas'
-]
-
-# Selecting columns to use for classification
-selected_columns = ['county', 'pfizer_doses', 'moderna_doses', 'jj_doses']
-
-without = df.loc[df['county'].isin(county_list)]
-for i, county in enumerate(county_list):
-    without.loc[df['county'] == county, 'county'] = i
-
-without = without[selected_columns]
-
-show_heatmap(without)
-fig, axes = plt.subplots(2, 2, figsize=(10, 6))
+without, range = load_by_county()
+show_heatmap(without, 'Vaccine usage by counties Calaveras and Plumas, CA')
+fig, axes = plt.subplots(2, 2, figsize=(10, 6), sharey=True)
+axes[0,0].set(xlabel=" ", title="Pfizer dose usage")
+axes[0,1].set(xlabel=" ", title="Moderna dose usage")
+axes[1,0].set(xlabel=" ", title="JJ dose usage")
 sns.histplot(without, ax=axes[0,0], x="pfizer_doses", kde=True, color='r')
 sns.histplot(without, ax=axes[0,1], x="moderna_doses", kde=True, color='g')
 sns.histplot(without, ax=axes[1,0], x="jj_doses", kde=True, color='b')
+# ax = plt.axes()
+# ax.set_title('Vaccine usage by maker company')
+fig.suptitle("Vaccine usage by maker company in Calaveras and Plumas, CA")
 plt.show()
--- mid-project/visualize.py
+++ mid-project/visualize.py
@@ -1,14 +1,16 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def show_heatmap(df, center="dark", hneg=250, hpos=354, s=80, l=60, corr_method="pearson"):
+def show_heatmap(df, title, center="dark", hneg=250, hpos=354, s=80, l=60, corr_method="pearson"):
+    ax = plt.axes()
     corr = df.corr(method=corr_method)
     cmap = sns.diverging_palette(hneg, hpos, s, l, center=center, as_cmap=True)
     sns.heatmap(corr, vmax=1, vmin=-.5, cmap=cmap, square=True, linewidths=.2)
+    ax.set_title(title)
     plt.show()
 
 # WIP
-def show_histogram(df, xlabels=[], rows=1, cols=1):
+def show_histogram(df, title, xlabels=[], rows=1, cols=1):
     print(xlabels)
     colors = ['r', 'g', 'b']
     current_label = 0
@@ -19,4 +21,6 @@ def show_histogram(df, xlabels=[], rows=1, cols=1):
         for r in range(rows):
             for c in range(cols):
                 sns.histplot(df,ax=axes[r, c], x=xlabels[current_label], kde=True, color=colors[(r + c) % len(colors)])
+    ax = plt.axes()
+    ax.set_title(title)
     plt.show()
