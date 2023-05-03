45575787b231300f7c4e7fdc84fb6636385272ef
Vladimir Ventura <vladventura@outlook.es>
2023-05-03 14:04:05 -0400

    Small changes
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

--- mid-project/logistic-regression-driver.py
+++ mid-project/logistic-regression-driver.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from logistic_regression import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 
-without, feature_range = load_by_county(selected_columns=['county', 'total_doses', 'cumulative_total_doses'])
+without, feature_range = load_by_county()
 print(without)
 X = without.iloc[:, 1:].values
 y = without.iloc[:,0].values
@@ -21,6 +21,8 @@ y_pred = logreg.predict(X_test)
 
 y_test = np.array(y_test).astype("float32")
 y_pred = np.array(y_pred).astype("float32")
+print("\t\tLogistic Regression")
+print("\tLogistic Regression -> Classification Report")
 print(classification_report(y_test, y_pred))
-print("Confusion Matrix")
+print("\tLogistic Regression -> Confusion Matrix")
 print(confusion_matrix(y_test, y_pred))
