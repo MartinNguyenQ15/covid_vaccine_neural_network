df81bf216f3d75b03f7d739a67286cfda990147c
Vladimir Ventura <vladventura@outlook.es>
2023-05-03 20:54:35 -0400

    Adding final project stuff
    Signed-off-by: Vladimir Ventura <vladventura@outlook.es>

---
+++ final-project/final-project-driver.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.regularizers import l2
+
+df = pd.read_csv('./cali_dataset.csv')
+selected_columns = ['county', 'administered_date', 'pfizer_doses', 'jj_doses', 'moderna_doses']
+without = df[selected_columns]
+
+without = without[without['county'] == 'Calaveras']
+without = without.loc[without['administered_date'].str.contains(r'|'.join(['2022', '2021', '2020']), regex=True)]
+
+features = without.drop(columns=['pfizer_doses', 'county'], axis=1)
+labels = without['pfizer_doses']
+
+features['administered_date'] = pd.to_datetime(features['administered_date'])
+features['administered_date'] = features['administered_date'].apply(lambda x: x.toordinal())
+
+X = pd.get_dummies(features)
+print(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(X_train.shape[1],), kernel_regularizer=l2(0.01)),
+    Dense(64, activation='relu', kernel_regularizer=l2(0.01)),
+    Dense(1),
+])
+
+model.compile(optimizer='adam', loss='mse')
+history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
+y_pred = model.predict(X_test)
+
+print("Pred:", y_pred[:10])
+print("Actual", y_test[:10])
+
+loss = model.evaluate(X_test, y_test)
+
+predicted_rates = model.predict(X_test)
+
+num_sims = 10
+
+sim_results = np.zeros(num_sims)
+alpha = 6.985
+
+for i in range(num_sims):
+    vac_rates = np.random.normal(predicted_rates.mean(), predicted_rates.std(), size=len(X_test))
+    vac_counts = vac_rates * alpha
+    sim_results[i] = vac_counts.sum()
+
+mean = sim_results.mean()
+std = sim_results.std()
+
+print(mean)
+print(sum(y_pred))
+print(sim_results)
